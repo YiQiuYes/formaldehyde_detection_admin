d9acce26f5acb0656e49f190f8e7193dcfaf55af
YiQiu <binzixiang@163.com>
2025-03-30 16:48:31 +0800

    chore(build): 更新 ndkVersion 至 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.formaldehyde_detection"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
