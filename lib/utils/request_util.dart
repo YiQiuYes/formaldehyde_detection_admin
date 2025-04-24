495ff7f776d3ecdc27aa13774729d5e849b469b6
YiQiu <binzixiang@163.com>
2025-04-24 21:39:24 +0800

    feat(request_util): 添加登录失效拦截器以处理401响应
    在`RequestUtil`类中新增`_addLoginInvalidInterceptor`方法，用于处理401响应。当检测到401状态码时，尝试使用刷新令牌重新获取访问令牌。若刷新失败或刷新令牌不存在，则跳转至登录页面。该功能增强了应用的安全性和用户体验。

--- lib/utils/request_util.dart
+++ lib/utils/request_util.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:formaldehyde_detection/pages/route_config.dart';
 import 'package:formaldehyde_detection/state/global_logic.dart';
 import 'package:formaldehyde_detection/utils/logger_util.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class RequestUtil {
   RequestUtil._privateConstructor();
@@ -19,6 +21,7 @@ class RequestUtil {
   Dio getDio() {
     _dio ??= Dio(BaseOptions(baseUrl: "http://134.175.119.27:8015"));
     _addTokenInterceptor(_dio!);
+    _addLoginInvalidInterceptor(_dio!);
     return _dio!;
   }
 
@@ -44,7 +47,7 @@ class RequestUtil {
       return Response(
         data: {"code": 500, "msg": "请求失败: $e"},
         statusCode: 200,
-        requestOptions: RequestOptions(path: url), 
+        requestOptions: RequestOptions(path: url),
       );
     }
   }
@@ -120,13 +123,79 @@ class RequestUtil {
   /// 返回值为void
   void _addTokenInterceptor(Dio dio) {
     final logic = Get.find<GlobalLogic>();
-    
+
     dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) {
           // 在这里添加token
           options.headers['Authorization'] = logic.getToken();
           return handler.next(options);
+        },
+      ),
+    );
+  }
+
+  /// 添加登录失效拦截器
+  /// [dio] dio对象
+  /// 返回值为void
+  void _addLoginInvalidInterceptor(Dio dio) {
+    dio.interceptors.add(
+      QueuedInterceptorsWrapper(
+        onResponse: (response, handler) {
+          // 在这里处理登录失效的情况
+          if (response.data['code'] == 401) {
+            final logic = Get.find<GlobalLogic>();
+            String? refreshToken = logic.getRefreshToken();
+            if (refreshToken == null) {
+              // 跳转到登录页面
+              Get.offAllNamed(RouteConfig.login);
+              return handler.next(response);
+            }
+
+            // 刷新token
+            Map<String, dynamic> params = {"token": refreshToken};
+            get("/user/refresh", params: params).then((value) async {
+              if (value.data['code'] == 200) {
+                String token = value.data['data']['token'];
+                logic.setToken(token); // 更新token
+                response.requestOptions.headers['Authorization'] = token;
+
+                // 刷新成功，重新发送请求
+                Response retryResponse = await getDio().request(
+                  response.requestOptions.path,
+                  data: response.requestOptions.data,
+                  queryParameters: response.requestOptions.queryParameters,
+                  options: Options(
+                    method: response.requestOptions.method,
+                    headers: response.requestOptions.headers,
+                    extra: response.requestOptions.extra,
+                    contentType: response.requestOptions.contentType,
+                    responseType: response.requestOptions.responseType,
+                    sendTimeout: response.requestOptions.sendTimeout,
+                    receiveTimeout: response.requestOptions.receiveTimeout,
+                    preserveHeaderCase:
+                        response.requestOptions.preserveHeaderCase,
+                    validateStatus: response.requestOptions.validateStatus,
+                    followRedirects: response.requestOptions.followRedirects,
+                    maxRedirects: response.requestOptions.maxRedirects,
+                    receiveDataWhenStatusError:
+                        response.requestOptions.receiveDataWhenStatusError,
+                    requestEncoder: response.requestOptions.requestEncoder,
+                    responseDecoder: response.requestOptions.responseDecoder,
+                    listFormat: response.requestOptions.listFormat,
+                    persistentConnection:
+                        response.requestOptions.persistentConnection,
+                  ),
+                );
+
+                handler.resolve(retryResponse);
+              } else {
+                // 跳转到登录页面
+                Get.offAllNamed(RouteConfig.login);
+              }
+            });
+          }
+          return handler.next(response);
         },
       ),
     );
